c6bc39ac0269b4aeb3d1ae89df0bd7ab51b4560d
mashkarazykrashka <47003132+mashkarazykrashka@users.noreply.github.com>
2019-09-30 14:55:45 +0300

    int

--- src/CalcInterface.php
+++ src/CalcInterface.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
-interface CalcInterface {
-
-    public function sqr(float $a, float $b);
-
-    public function sqrt(float $a, float $b);
-
-}
+interface CalcInterface 
+    {
+        public function plus(float $x, float $y): float;
+        public function minus(float $x, float $y): float;
+        public function multiplication(float $x, float $y): float;
+        public function division(float $x, float $y): float;
+        public function squared(float $x): float;
+        public function cubed(float $x): float;
+        public function squareRoot(float $x): float;
+        public function cubeRoot(float $x): float;
+        public function ythPower(float $x, float $y): float;
+        public function ythRoot(float $x, float $y): float;
+    }
